076a2be2a2b3709cc696b6f7d031175de9b6ad3f
gonghojin <yolip51@naver.com>
2022-09-09 21:16:46 +0900

    Add: webflux 기본 테스트코드 사용법 학습

--- microservices/product-service/src/test/java/com/gongdel/microservices/core/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/com/gongdel/microservices/core/product/PersistenceTests.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.test.StepVerifier;
 
@@ -48,6 +50,58 @@ public class PersistenceTests {
 				.verifyComplete();
 
 		StepVerifier.create(repository.count()).expectNext(2l).verifyComplete();
+	}
+
+	@Test
+	public void update() {
+		// Given
+		savedEntity.setName("n2");
+
+		// When
+		// Then
+		StepVerifier.create(repository.save(savedEntity))
+				.expectNextMatches(updatedEntity -> updatedEntity.getName().equals("n2"))
+				.verifyComplete();
+
+		StepVerifier.create(repository.findById(savedEntity.getId()))
+				.expectNextMatches(foundEntity -> foundEntity.getVersion() == 1 && foundEntity.getName().equals("n2"))
+				.verifyComplete();
+	}
+
+	@Test
+	public void delete() {
+		StepVerifier.create(repository.delete(savedEntity)).verifyComplete();
+		StepVerifier.create(repository.existsById(savedEntity.getId())).expectNext(false).verifyComplete();
+	}
+
+	@Test
+	public void duplicateError() {
+		ProductEntity entity = new ProductEntity(savedEntity.getProductId(), "n", 1);
+		StepVerifier.create(repository.save(entity)).expectError(DuplicateKeyException.class).verify();
+	}
+
+	@Test
+	public void optimisticLockError() {
+		// Store the saved entity in two separate entity objects
+		ProductEntity entity1 = repository.findById(savedEntity.getId()).block();
+		ProductEntity entity2 = repository.findById(savedEntity.getId()).block();
+
+		// Update the entity using the first entity object
+		entity1.setName("n1");
+		repository.save(entity1).block();
+
+		//  Update the entity using the second entity object.
+		// This should fail since the second entity now holds a old version number, i.e. a Optimistic Lock Error
+		StepVerifier.create(repository.save(entity2))
+				.expectError(OptimisticLockingFailureException.class)
+				.verify();
+
+		// Get the updated entity from the database and verify its new sate
+		StepVerifier.create(repository.findById(savedEntity.getId()))
+				.expectNextMatches(foundEntity ->
+						foundEntity.getVersion() == 1 &&
+								foundEntity.getName().equals("n1"))
+				.verifyComplete();
 	}
 
 	private boolean areProductEqual(ProductEntity expectedEntity, ProductEntity actualEntity) {
