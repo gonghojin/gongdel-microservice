5da80c3b671abf185ed84f6fcefd9be914c57acd
gonghojin <yolip51@naver.com>
2022-09-11 17:15:41 +0900

    Add: RecommendationService C R D

---
+++ api/src/main/java/com/gongdel/microservices/api/core/recommendation/RecommendationService.java
@@ -0,0 +1,25 @@
+package com.gongdel.microservices.api.core.recommendation;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+
+public interface RecommendationService {
+
+	@PostMapping(
+			value    = "/recommendation",
+			consumes = "application/json",
+			produces = "application/json")
+	Recommendation createRecommendation(@RequestBody Recommendation body);
+
+	@GetMapping(
+			value    = "/recommendation",
+			produces = "application/json")
+	Flux<Recommendation> getRecommendations(@RequestParam(value = "productId", required = true) int productId);
+
+	@DeleteMapping(value = "/recommendation")
+	void deleteRecommendations(@RequestParam(value = "productId", required = true)  int productId);
+}
