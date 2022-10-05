aa6f3dd529eb5b86efe9cda4a640b2a810f393a9
gonghojin <yolip51@naver.com>
2022-10-06 01:10:35 +0900

    Add: 클라우드 게이트웨이로 msa 상태체크 api 옮기, 게이트웨이를 통해 내부 api 연결하기

---
+++ spring-cloud/gateway/src/main/java/com/gongdel/gateway/HealthCheckConfiguration.java
@@ -0,0 +1,62 @@
+package com.gongdel.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthIndicator;
+import org.springframework.boot.actuate.health.DefaultReactiveHealthIndicatorRegistry;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthAggregator;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicatorRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.LinkedHashMap;
+
+@Configuration
+public class HealthCheckConfiguration {
+
+	private final WebClient.Builder webClientBuilder;
+	private final HealthAggregator healthAggregator;
+	private WebClient webClient;
+
+	@Autowired
+	public HealthCheckConfiguration(
+			WebClient.Builder webClientBuilder,
+			HealthAggregator healthAggregator
+	) {
+		this.webClientBuilder = webClientBuilder;
+		this.healthAggregator = healthAggregator;
+	}
+
+	@Bean
+	ReactiveHealthIndicator healthIndicatorMicroservices() {
+		ReactiveHealthIndicatorRegistry registry =
+				new DefaultReactiveHealthIndicatorRegistry(new LinkedHashMap<>());
+
+		registry.register("product", () -> getHealth("http://product"));
+		registry.register("recommendation", () -> getHealth("http://recommendation"));
+		registry.register("review", () -> getHealth("http://review"));
+		registry.register("product-composite", () -> getHealth("http://product-composite"));
+
+		return new CompositeReactiveHealthIndicator(healthAggregator, registry);
+	}
+
+	private Mono<Health> getHealth(String url) {
+		url += "/actuator/health";
+
+		return getWebClient().get().uri(url)
+				.retrieve().bodyToMono(String.class)
+				.map(s -> new Health.Builder().up().build())
+				.onErrorResume(ex -> Mono.just(new Health.Builder().down(ex).build()))
+				.log();
+	}
+
+	private WebClient getWebClient() {
+		if (webClient == null) {
+			webClient = webClientBuilder.build();
+		}
+		return webClient;
+	}
+}
