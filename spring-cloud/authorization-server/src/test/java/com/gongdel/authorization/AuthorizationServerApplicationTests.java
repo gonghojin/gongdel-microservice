3afd2bb3952d9f79389ca61fa835b50020327943
gonghojin <yolip51@naver.com>
2022-10-16 23:21:10 +0900

    Fix: 구성서로 환경설정 바꾸기 기본 베이스변경(테스트코드 및 환경설정)

--- spring-cloud/authorization-server/src/test/java/com/gongdel/authorization/AuthorizationServerApplicationTests.java
+++ spring-cloud/authorization-server/src/test/java/com/gongdel/authorization/AuthorizationServerApplicationTests.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = {"eureka.client.enabled=false"})
+@SpringBootTest(properties = {"eureka.client.enabled=false","spring.cloud.config.enabled=false"})
 @AutoConfigureMockMvc
 class AuthorizationServerApplicationTests {
 
