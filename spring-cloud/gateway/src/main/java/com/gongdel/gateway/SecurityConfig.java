58c8e8b38d60afc6ca4a3ed8a10e6ee813fa755e
gonghojin <yolip51@naver.com>
2022-10-19 23:18:26 +0900

    Add: 게이트웨이 시큐리티 권한 열어주기

--- spring-cloud/gateway/src/main/java/com/gongdel/gateway/SecurityConfig.java
+++ spring-cloud/gateway/src/main/java/com/gongdel/gateway/SecurityConfig.java
@@ -9,15 +9,19 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
-		httpSecurity
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http
+				.csrf().disable()
 				.authorizeExchange()
-				.pathMatchers("/actuator/**").permitAll() // 상용환경에서는  주의
+				.pathMatchers("/headerrouting/**").permitAll()
+				.pathMatchers("/actuator/**").permitAll()
+				.pathMatchers("/eureka/**").permitAll()
+				.pathMatchers("/oauth/**").permitAll()
+				.pathMatchers("/config/**").permitAll()
 				.anyExchange().authenticated()
 				.and()
 				.oauth2ResourceServer()
-				.jwt(); // JWT로 인코딩된 Oauth 2.0 접근 토큰을 기반으로 인증 및 권한 부여
-
-		return httpSecurity.build();
+				.jwt();
+		return http.build();
 	}
 }
